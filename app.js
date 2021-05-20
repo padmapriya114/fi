8c3714b36135b6265a11c096088cdf139ca4f82b
padmapriya114 <46240280+padmapriya114@users.noreply.github.com>
2021-05-20 09:27:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.get('/', (request, response) => {
   response.json({ info: 'Node.js, Express, and Postgres API' })
 })
 
-app.post('/', db.insertfi)
+app.post('/fi', db.insertfi)
 
 
 app.listen(7000, () => {
